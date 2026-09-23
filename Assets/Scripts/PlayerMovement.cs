77a47731aa3c4b3e6afa13bdd12a3d8bbc9de19e
William Hall Nanberg <117825883+MrPurpL@users.noreply.github.com>
2026-09-23 11:59:00 +0200

    Flying enemy, Player prefab
    Added flying enemies and made a player prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,6 +21,10 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private AudioClip[] jumpSounds;
     [SerializeField] private AudioClip walkSound;
     [SerializeField] private ParticleSystem jumpParticleSystem;
+    [SerializeField] private float playerPositionUpdateTimer;
+    [SerializeField] private GameObject lastPLayerPosition;
+    [SerializeField] private GameObject newPlayerPosition;
+    private bool playerPositionUpdateTimerReset = true;
     bool canMove = true;
     bool walkSoundTimer = false;
     private float playerPositionX;
@@ -70,6 +74,11 @@ public class PlayerMovement : MonoBehaviour
             StartCoroutine(WalkSoundTimer());
         }
         playerPositionX = newPlayerPositionX;
+
+        if (playerPositionUpdateTimerReset == true)
+        {
+            StartCoroutine(PlayerPosition());
+        }
     }
 
     private void FixedUpdate()
@@ -159,6 +168,15 @@ public class PlayerMovement : MonoBehaviour
         audioSource.PlayOneShot(walkSound);
         yield return new WaitForSeconds(walkSoundTimerTime);
         walkSoundTimer = false;
+    }
+
+    private IEnumerator PlayerPosition()
+    {
+        playerPositionUpdateTimerReset = false;
+        newPlayerPosition.transform.position = gameObject.transform.position;
+        yield return new WaitForSeconds(playerPositionUpdateTimer);
+        lastPLayerPosition.transform.position = newPlayerPosition.transform.position;
+        playerPositionUpdateTimerReset = true;
     }
 
 }
